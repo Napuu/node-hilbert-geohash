6cb136cb11fc822b2c0d8406ef8ddda1e95ea7b4
Santeri <santeri.j.kaariainen@gmail.com>
2020-08-30 14:34:03 +0000

    cleanup1

--- src/conversion.ts
+++ src/conversion.ts
@@ -3,48 +3,20 @@ type Base = "Base64" | "Base16" | "Base4";
 
 const IntConversion = {
   // https://stackoverflow.com/questions/6213227/fastest-way-to-convert-a-number-to-radix-64-in-javascript
-  Base64:
-    //   0       8       16      24      32      40      48      56     63
-    //   v       v       v       v       v       v       v       v      v
-    "0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz",
+  Base64: "0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz",
   Base16: "0123456789ABCDEF",
   Base4: "0123",
-  // You have the freedom, here, to choose the glyphs you want for
-  // representing your base-64 numbers. The ASCII encoding guys usually
-  // choose a set of glyphs beginning with ABCD..., but, looking at
-  // your update #2, I deduce that you want glyphs beginning with
-  // 0123..., which is a fine choice and aligns the first ten numbers
-  // in base 64 with the first ten numbers in decimal.
-
-  // This cannot handle negative numbers and only works on the
-  //     integer part, discarding the fractional part.
-  // Doing better means deciding on whether you're just representing
-  // the subset of javascript numbers of twos-complement 32-bit integers
-  // or going with base-64 representations for the bit pattern of the
-  // underlying IEEE floating-point number, or representing the mantissae
-  // and exponents separately, or some other possibility. For now, bail
   fromNumber: function (number: bigint, base: BaseNumber) {
-    //if (
-    //isNaN(Number(number)) ||
-    //number == null ||
-    //number == Number.POSITIVE_INFINITY
-    //)
-    //throw "The input is not valid";
-    if (number < 0n) throw "Can't represent negative numbers now";
-
-    var rixit; // like 'digit', only in some non-decimal radix
-    var residual = number;
-    var result = "";
+    let residual = number;
+    let result = "";
     while (true) {
-      rixit = Number(BigInt(residual) % BigInt(base));
-      result = this[("Base" + base) as Base].charAt(rixit) + result;
+      const digit = Number(BigInt(residual) % BigInt(base));
+      result = this[("Base" + base) as Base].charAt(digit) + result;
       residual = BigInt(residual) / BigInt(base);
-
       if (residual == 0n) break;
     }
     return result;
   },
-
   toNumber: function (string: string, base: BaseNumber) {
     var result = 0n;
     let chars = string.split("");
@@ -56,45 +28,29 @@ const IntConversion = {
     return result;
   },
 };
-export const encode_int = (n: bigint, bits_per_char = 6) => {
+
+export const encode_int = (n: bigint, bits_per_char = 6): string => {
   if (bits_per_char === 6) {
-    return encode_int64(n);
+    return IntConversion.fromNumber(n, 64);
   }
   if (bits_per_char === 4) {
-    return encode_int16(n);
+    return IntConversion.fromNumber(n, 16);
   }
   if (bits_per_char === 2) {
-    return encode_int4(n);
+    return IntConversion.fromNumber(n, 4);
   }
   return "";
 };
-export const decode_int = (n: string, bits_per_char = 6) => {
+
+export const decode_int = (n: string, bits_per_char = 6): bigint => {
   if (bits_per_char === 6) {
-    return decode_int64(n);
+    return IntConversion.toNumber(n, 64);
   }
   if (bits_per_char === 4) {
-    return decode_int16(n);
+    return IntConversion.toNumber(n, 16);
   }
   if (bits_per_char === 2) {
-    return decode_int4(n);
+    return IntConversion.toNumber(n, 4);
   }
-  return 0;
-};
-const encode_int64 = (n: bigint) => {
-  return IntConversion.fromNumber(n, 64);
-};
-const encode_int16 = (n: bigint) => {
-  return IntConversion.fromNumber(n, 16);
-};
-const encode_int4 = (n: bigint) => {
-  return IntConversion.fromNumber(n, 4);
-};
-const decode_int64 = (n: string) => {
-  return IntConversion.toNumber(n, 64);
-};
-const decode_int16 = (n: string) => {
-  return IntConversion.toNumber(n, 16);
-};
-const decode_int4 = (n: string) => {
-  return IntConversion.toNumber(n, 4);
+  return 0n;
 };
